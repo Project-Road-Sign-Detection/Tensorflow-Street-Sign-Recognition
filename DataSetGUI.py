4bea409250dec3550793e661f92962d1daca3e97
Vincent <VincentWack92@gmail.com>
2018-09-18 15:59:40 +0200

    Delete Images Function

--- DataSetGUI.py
+++ DataSetGUI.py
@@ -34,6 +34,11 @@ class DSGView(QWidget):
         self.dataset_btn.clicked.connect(self.on_dataset)
         self.grid.addWidget(self.dataset_btn, 7, 4)
 
+        self.delete_images_btn = QPushButton("Leere Bilder löschen")
+        self.delete_images_btn.setEnabled(False)
+        self.delete_images_btn.clicked.connect(self.on_delete_img)
+        self.grid.addWidget(self.delete_images_btn, 3, 1)
+
         self.setLayout(self.grid)
         self.setGeometry(300, 300, 700, 350)
         self.setWindowTitle('Data Set Generator')
@@ -41,7 +46,10 @@ class DSGView(QWidget):
     def on_set_path(self):
         self.directory = str(QFileDialog.getExistingDirectory(self, "Arbeitsverzeichnis wählen"))
         self.path.setText(self.directory)
+
         self.dataset_btn.setEnabled(True)
+        self.delete_images_btn.setEnabled(True)
+
         self.file_model.setRootPath(self.directory)
         self.tree_view.setModel(self.file_model)
         self.tree_view.setRootIndex(self.file_model.index(self.directory))
@@ -51,6 +59,10 @@ class DSGView(QWidget):
         self.generator = Generator(self.directory)
         print(self.generator.__class__)
         self.generator.createDataSetZIP()
+
+    def on_delete_img(self):
+        print(self.file_model.filePath(self.file_model.index()))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- Generator.py
+++ Generator.py
@@ -69,15 +69,17 @@ class Generator():
         
         self.class_score = self._calculateClassScore()
         
-        
-    def deleteEmptyImages(self):
-        for p, dirs, filenames in os.walk(self.PATH):
+    def deleteEmptyImages(self, path = None):
+        if not path:
+            path = self.PATH
+
+        for p, dirs, filenames in os.walk(path):
             for file in [f for f in filenames if f[-3:]=='png']:
                 if file[:-3]+'xml' not in self.label_names:
                     os.remove(os.path.join(p,file))
                     print("%i deleted due to missing label!" %(os.path.join(p,file)))
-                    
-    def _calculateClassScore(self):
+
+    def _calculateClassScore(self,):
         class_score = {}
         
         for label in self.label_paths:
