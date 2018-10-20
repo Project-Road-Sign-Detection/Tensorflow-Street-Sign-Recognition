9c610cebbf129ee8639d3ffe2db80537daa87a1f
Dennis <dennis.sommer@web.de>
2018-10-21 00:27:13 +0800

    Output

---
+++ Weitere Skripte/sizecheck.py
@@ -0,0 +1,17 @@
+import cv2
+
+# read image
+img = cv2.imread('00833.png', cv2.IMREAD_UNCHANGED)
+ 
+# get dimensions of image
+dimensions = img.shape
+ 
+# height, width, number of channels in image
+height = img.shape[0]
+width = img.shape[1]
+channels = img.shape[2]
+ 
+print('Image Dimension    : ',dimensions)
+print('Image Height       : ',height)
+print('Image Width        : ',width)
+print('Number of Channels : ',channels)
