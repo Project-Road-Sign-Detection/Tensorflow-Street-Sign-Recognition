78f8817ea079c18c161be945b84e081154bac2e5
Vincent - Windows <VincentWack92@gmail.com>
2018-10-20 18:40:31 +0200

    FileSize Check Loop

--- Weitere Skripte/sizecheck.py
+++ Weitere Skripte/sizecheck.py
@@ -1,17 +1,23 @@
 import cv2
+import os, csv
 
-# read image
-img = cv2.imread('00833.png', cv2.IMREAD_UNCHANGED)
- 
-# get dimensions of image
-dimensions = img.shape
- 
-# height, width, number of channels in image
-height = img.shape[0]
-width = img.shape[1]
-channels = img.shape[2]
- 
-print('Image Dimension    : ',dimensions)
-print('Image Height       : ',height)
-print('Image Width        : ',width)
-print('Number of Channels : ',channels)
+with open(os.path.join(os.getcwd(),'FileSizes.csv'),'w', newline='') as out:
+    writer = csv.writer(out, delimiter=',', quoting=csv.QUOTE_NONE)
+    writer.writerow(['FileName', 'Height', 'Width', 'Channel'])
+
+    for p, dirs, filenames in os.walk('PATH'):
+
+        # read image
+        for file in filenames:
+            if file[-3:] == 'png':
+                img = cv2.imread(file, cv2.IMREAD_UNCHANGED)
+
+                # get dimensions of image
+                dimensions = img.shape
+
+                # height, width, number of channels in image
+                height = img.shape[0]
+                width = img.shape[1]
+                channels = img.shape[2]
+
+                writer.writerow([file,img.shape[0],img.shape[1],img.shape[2]])
